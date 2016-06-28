ee9cfa223a64ba491ff0974ae02db7cdfc100fac
josevadri <josevayac@gmail.com>
2016-06-27 18:56:51 -0600

    Subida de proyecto

---
+++ QuizCurri/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizCurri.Startup))]
+namespace QuizCurri
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
